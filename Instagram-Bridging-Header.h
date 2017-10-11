49dde6c354557fadc84c06aaf407e5db52ce3a64
米住直親 <beijuujikaoya@imac.local>
2017-10-11 22:11:58 +0900

    Initial Commit

---
+++ Instagram-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by 米住直親 on 2017/07/29.
+//  Copyright © 2017年 naochika.yonezumi. All rights reserved.
+//
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+#endif
+/* Instagram_Bridging_Header_h */
+
---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 米住直親 on 2017/08/01.
+//  Copyright © 2017年 naochika.yonezumi. All rights reserved.
+//
+
+import Foundation
+    struct Const {
+        static let PostPath = "posts"
+        static let commentPath = "comment"
+    }
+
